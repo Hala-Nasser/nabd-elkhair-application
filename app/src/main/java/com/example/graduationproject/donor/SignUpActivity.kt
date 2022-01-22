9e712ae43009b899a176f9e4d813bc5205801ae8
Hala-Nasser <lola.el3sola2000@gmail.com>
2022-01-22 10:59:13 +0200

    charity item

--- app/src/main/java/com/example/graduationproject/donor/SignUpActivity.kt
+++ app/src/main/java/com/example/graduationproject/donor/SignUpActivity.kt
@@ -3,12 +3,18 @@ package com.example.graduationproject.donor
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
 import com.example.graduationproject.classes.GeneralChanges
 import com.example.graduationproject.R
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    lateinit var spinner: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -22,5 +28,25 @@ class SignUpActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.sign_in).setOnClickListener {
             GeneralChanges().prepareFadeTransition(this, SignInActivity())
         }
+
+        spinner = findViewById(R.id.location_spinner)
+        spinner.onItemSelectedListener = this
+        ArrayAdapter.createFromResource(
+                this,
+                R.array.donor_location_array,
+                android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        spinner.setSelection(p2)
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        spinner.setSelection(0)
     }
 }
